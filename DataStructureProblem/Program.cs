e33fc486cbf4ebf232f1fbf97aa4bfe39af8222f
mohan338 <sahumohan338@gmail.com>
2022-05-21 16:58:49 +0530

    [mohan]add:Add Data in LinkedList

--- DataStructureProblem/Program.cs
+++ DataStructureProblem/Program.cs
@@ -6,9 +6,10 @@ public class Program
     public static void Main(String[] args)
     {
         LinkedList list = new LinkedList();
-        list.Add(70);
+       
         list.Add(56);
         list.Add(30);
+        list.Add(70);
         list.Display();
     }
 }
